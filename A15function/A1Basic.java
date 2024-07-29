dedd11c9cd349f4992e3f96e31cf7ce8ace5d09d
sachin <sachinnarofficials@gmail.com>
2024-07-29 07:38:55 +0530

    started function and method

---
+++ A15function/A1Basic.java
@@ -0,0 +1,13 @@
+package A15function;
+
+public class A1Basic {
+    public static void printHelloWorld() {
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+    }
+
+    public static void main(String args[]) {
+        printHelloWorld();
+    }
+}
