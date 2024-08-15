ec0a72a6abe940ed2f0891c0893e3bdae0a34296
sachin <sachinnarofficials@gmail.com>
2024-08-15 07:04:43 +0530

    hallow rectrangel pattern

--- A16AdvancePatterns/A1HallowRectrangle.java
+++ A16AdvancePatterns/A1HallowRectrangle.java
@@ -4,12 +4,11 @@ public class A1HallowRectrangle {
     public static void HallowRectangle(int row, int col) {
         for (int i = 1; i <= row; i++) {
             for (int j = 1; j <= col; j++) {
-                if (i == 1 || i == row || j == 1 || j == col) {
-                    System.out.print('*');
+                if (i == 1 || j == 1 || i == row || j == col) {
+                    System.out.print("*");
                 } else {
-                    System.out.print(' ');
+                    System.out.print(" ");
                 }
-
             }
             System.out.println(' ');
         }
