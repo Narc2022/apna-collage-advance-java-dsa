d4de035b7849c827366268a4b3b08d0a93e1bd6c
sachin <sachinnarofficials@gmail.com>
2024-08-26 17:33:04 +0530

    binary search

--- A15function/A11BinaryToDecimal.java
+++ A15function/A11BinaryToDecimal.java
@@ -5,17 +5,16 @@ public class A11BinaryToDecimal {
     public static void binToDec(int binNum) {
         int pow = 0;
         int decNum = 0;
-
         while (binNum > 0) {
             int lastNum = binNum % 10;
-            decNum = decNum + lastNum * ((int) Math.pow(2, pow));
-            binNum = binNum / 10;
+            decNum = decNum + lastNum * (int) Math.pow(2, pow);
             pow++;
+            binNum = binNum / 10;
         }
         System.out.println(decNum);
     }
 
     public static void main(String args[]) {
-        binToDec(110110101);
+        binToDec(1101);
     }
 }
